2ff77bca6b4b699e98a3a7375992dccfaad975e2
Masum Billah <masum.it@eongroup.com.bd>
2023-04-02 00:47:38 +0600

    fix css

--- education/index.php
+++ education/index.php
@@ -1,22 +1,29 @@
 <?php 
   include('../components/header.php')
 ?>
-<div class="row university jumbotron">
-  <div class="col-lg-12 ">
-    <h1>Education</h1>
-    <h2>IsDB-BISEW Scholarship Project</h2>
-    <p>
-      <i class="fa fa-graduation-cap" aria-hidden="true"> Diploma in Web Development.</i><br>
-    </p>
-    <h2>Bangladesh Islami University</h2>
-    <p>
-      <i class="fa fa-graduation-cap" aria-hidden="true"> B.A. in Islamic Studies.</i><br>
-    </p>
-    <h2>Islamic University</h2>
-    <p>
-      <i class="fa fa-graduation-cap" aria-hidden="true"> Kamil (M.A.).</i><br>
-      <i class="fa fa-graduation-cap" aria-hidden="true"> Fazil (B.A.).</i>
-    </p>
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-lg-12  edu">
+      <img src="../assets/images/edu.jpg" alt="">
+    </div>
+  </div>
+  <div class="row university jumbotron">
+    <div class="col-lg-12 ">
+      <h1>Education</h1>
+      <h2>IsDB-BISEW Scholarship Project</h2>
+      <p>
+        <i class="fa fa-graduation-cap" aria-hidden="true"> Diploma in Web Development.</i><br>
+      </p>
+      <h2>Bangladesh Islami University</h2>
+      <p>
+        <i class="fa fa-graduation-cap" aria-hidden="true"> B.A. in Islamic Studies.</i><br>
+      </p>
+      <h2>Islamic University</h2>
+      <p>
+        <i class="fa fa-graduation-cap" aria-hidden="true"> Kamil (M.A.).</i><br>
+        <i class="fa fa-graduation-cap" aria-hidden="true"> Fazil (B.A.).</i>
+      </p>
+    </div>
   </div>
 </div>
 <?php 
