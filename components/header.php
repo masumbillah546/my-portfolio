b47f36a75bdd0508fbf2a894713610b05bdf69db
Masum Billah <masum.it@eongroup.com.bd>
2023-04-02 00:15:04 +0600

    refactor

---
+++ components/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>My Portfolio</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+  <link rel="stylesheet" href="../assets/css/style.css">
+  <link rel="stylesheet" href="../assets/font-awesome/css/font-awesome.min.css">
+  <script src="../assets/js/jquery.min.js"></script>
+  <script src="../assets/js/bootstrap.min.js"></script>
+</head>
+<body >
+  <nav class="container-fluid navbar-inverse navbar-fixed-top text-uppercase ">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle " data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Portfolio</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="../index.php#profile">About</a></li>
+        <li><a href="../index.php#works">Skills</a></li>	        
+        <li><a href="../cssp.html">PROJECTS</a></li>
+        <li><a href="../education">Education</a></li>
+        <li><a href="../index.php#form">Contact</a></li>	        
+      </ul>
+    </div>
+  </nav>
--- components/skills.php
+++ components/skills.php
@@ -5,28 +5,28 @@
 		<div class="col-lg-3 text-center" >HTML5
 			<div class="bg-info">
 				<a href="">
-					<img class="img-responsive" src="images/html5.jpg" alt="image">
+					<img class="img-responsive" src="../assets/images/html5.jpg" alt="image">
 				</a>
 			</div> 
 		</div>
 		<div class="col-lg-3 text-center">CSS3
 			<div class="bg-info">
 				<a href="cssp.html">
-					<img class="img-responsive" src="images/css.jpg" alt="image">
+					<img class="img-responsive" src="../assets/images/css.jpg" alt="image">
 				</a>
 			</div>
 		</div>
 		<div class="col-lg-3 text-center">JavaScript
 			<div class="bg-info">
 				<a href="">
-					<img class="img-responsive" src="images/js.jpg" alt="image">
+					<img class="img-responsive" src="../assets/images/js.jpg" alt="image">
 				</a>
 			</div> 
 		</div>
 		<div class="col-lg-3 text-center">Bootstrap3
 			<div class="bg-info">
 				<a href="">
-					<img class="img-responsive" src="images/bs.jpg" alt="image">
+					<img class="img-responsive" src="../assets/images/bs.jpg" alt="image">
 				</a>
 			</div><br>
 		</div>
@@ -35,28 +35,28 @@
 		<div class="col-lg-3 text-center">NodeJs
 			<div class="bg-info">
 				<a href="">
-					<img class="img-responsive" src="images/gg.jpg" alt="image">
+					<img class="img-responsive" src="../assets/images/gg.jpg" alt="image">
 				</a>
 			</div>
 		</div>
 		<div class="col-lg-3 text-center">Laravel
 			<div class="bg-info">
 				<a href="">
-					<img class="img-responsive" src="images/h.png" alt="image">
+					<img class="img-responsive" src="../assets/images/h.png" alt="image">
 				</a>
 			</div><br>
 		</div>
 		<div class="col-lg-3 text-center">Graphics design
 			<div class="bg-info">
 				<a href="">
-					<img class="img-responsive" src="images/b.jpg" alt="image">
+					<img class="img-responsive" src="../assets/images/b.jpg" alt="image">
 				</a>
 			</div>
 		</div>
 		<div class="col-lg-3 text-center">Photoshop
 			<div class="bg-info">
 				<a href="">
-					<img class="img-responsive" src="images/ps.jpg" alt="image">
+					<img class="img-responsive" src="../assets/images/ps.jpg" alt="image">
 				</a>
 			</div>
 		</div>
