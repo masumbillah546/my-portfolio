d04ce24138d8357db2f1bccf4a55685264c24b27
Masum Billah <masum.it@eongroup.com.bd>
2023-04-02 02:25:08 +0600

    smooth scroll

--- components/footer.php
+++ components/footer.php
@@ -1,51 +1,43 @@
 
 <!-- Footer -->
-  <footer class="footer text-center">
-    <div class="container">
-      <div class="row">
+<footer class="footer text-center">
+  <div class="container">
+    <div class="row">
 
-        <!-- Footer Location -->
-        <div class="col-lg-4">
-        	<br>
-          <h4 class="text-uppercase">Location</h4>
-          <hr>
-          <p class="lead">Dhaka, Bangladesh</p>
-        </div>
-
-        <!-- Footer Social Icons -->
-        <div class="col-lg-4">
-          <h4 class="text-uppercase">Get in touch</h4>
-          <a class="btn btn-danger" href="https://www.facebook.com/masum.billah.7355079">
-            <i class="fa fa-facebook-square" aria-hidden="true"></i>
-          </a>
-          <a class="btn btn-danger" href="https://twitter.com/Masumbillah546">
-          	<i class="fa fa-twitter" aria-hidden="true"></i>
-          </a>
-          <a class="btn btn-danger" href="https://www.linkedin.com/in/masum-billah-35756817b/">
-            <i class="fa fa-linkedin-square"></i>
-          </a>
-          <a class="btn btn-danger" href="https://www.youtube.com/channel/UC5HOZf7S1_mp88M89fdCotQ?view_as=subscriber">
-            <i class="fa fa-youtube" aria-hidden="true"></i>
-          </a>
-        </div>
-
-        <!-- Footer About Text -->
-        <div class="col-lg-4">
-        </div>
-
+      <!-- Footer Location -->
+      <div class="col-lg-4">
+        <br>
+        <h4 class="text-uppercase">Location</h4>
+        <hr>
+        <p class="lead">Dhaka, Bangladesh</p>
       </div>
-    </div>
-  </footer>
 
-      
- <!--    <footer class="container-fluid footer">
-      	<div class="row">
-         <div class=" col-lg-12 text-center" ><p>Footer</p></div>
-        </div>
-    </footer>
- -->
+      <!-- Footer Social Icons -->
+      <div class="col-lg-4">
+        <h4 class="text-uppercase">Get in touch</h4>
+        <a class="btn btn-danger" href="https://www.facebook.com/masum.billah.7355079">
+          <i class="fa fa-facebook-square" aria-hidden="true"></i>
+        </a>
+        <a class="btn btn-danger" href="https://twitter.com/Masumbillah546">
+          <i class="fa fa-twitter" aria-hidden="true"></i>
+        </a>
+        <a class="btn btn-danger" href="https://www.linkedin.com/in/masum-billah-35756817b/">
+          <i class="fa fa-linkedin-square"></i>
+        </a>
+        <a class="btn btn-danger" href="https://www.youtube.com/channel/UC5HOZf7S1_mp88M89fdCotQ?view_as=subscriber">
+          <i class="fa fa-youtube" aria-hidden="true"></i>
+        </a>
+      </div>
+
+      <!-- Footer About Text -->
+      <div class="col-lg-4">
+      </div>
+
+    </div>
+  </div>
+</footer>
        
-<!-- <script>
+<script>
 $(document).ready(function(){
   // Add scrollspy to <body>
   $('body').scrollspy({target: ".navbar", offset: 50});   
@@ -72,8 +64,6 @@ $(document).ready(function(){
     }  // End if
   });
 });
-</script> -->
-
-
+</script>
 </body>
 </html>
