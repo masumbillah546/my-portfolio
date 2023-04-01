801373c1d676c09e4429b535e9654ce203be60ad
Masum Billah <masum.it@eongroup.com.bd>
2023-04-02 01:10:12 +0600

    refactor

--- components/education.php
+++ components/education.php
@@ -1,4 +1,4 @@
-<div id="education" class="container-fluid position-relative">
+<div id="education" class="container-fluid">
   <div class="row">
     <div class="col-lg-12  edu">
       <img src="../assets/images/edu.jpg" alt="">
