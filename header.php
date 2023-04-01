242a1017d13d43ff95433d195b1b199813591083
Masum Billah <masum.it@eongroup.com.bd>
2023-04-01 22:55:27 +0600

    add php

---
+++ header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>My Portfolio</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+  <script src="js/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+</head>
+<body >
+
+
+
+
+    <nav class="container-fluid navbar-inverse navbar-fixed-top text-uppercase ">
+
+     <div class="navbar-header"><!---header start---->
+      <button type="button" class="navbar-toggle " data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+          <a class="navbar-brand" href="#">Portfolio</a>
+     </div><!---header end---->
+
+      <div class="collapse navbar-collapse" id="myNavbar">
+	      <ul class="nav navbar-nav">
+	        <li class="active"><a href="#profile">About</a></li>
+	        <li><a href="skills.html#works">Skills</a></li>	        
+	        <li><a href="cssp.html">PROJECTS</a></li>
+	        <li><a href="education.html">Education</a></li>
+	        <li><a href="contact.html#form">Contact</a></li>	        
+	      </ul>
+	   </div>
+
+    </nav>
